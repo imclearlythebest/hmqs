f4eda05f66d3c200854eb6a77b76a76a21b50137
cyyara <noshinshaiara@gmail.com>
2026-04-08 05:32:00 +0600

    Add models to db

--- WebApp/Data/WebAppDbContext.cs
+++ WebApp/Data/WebAppDbContext.cs
@@ -6,6 +6,10 @@ namespace WebApp.Data
 {
     public class WebAppDbContext(DbContextOptions<WebAppDbContext> options) : IdentityDbContext<WebAppUser>(options)
     {
+        public DbSet<Track> Tracks => Set<Track>();
+        public DbSet<Artist> Artists => Set<Artist>();
+        public DbSet<Collection> Albums => Set<Collection>();
+        public DbSet<Genre> Genres => Set<Genre>();
         public DbSet<Scrobble> Scrobbles => Set<Scrobble>();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
